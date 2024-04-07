b8cb2701454c05cdcf012f02825e98297e152c6d
Tiến Trần <tienco201@gmail.com>
2024-04-07 15:15:47 +0700

    config file

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    timezone: '+7:00',
+  },
+};
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "dksmart",
+//     "host": "127.0.0.1",
+//     "port": "3306",
+//     "dialect": "mysql",
+//     "logging": false,
+//     "timezone": "+7:00"
+//   }
+// }
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -5,7 +5,7 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
-  null,
+  process.env.DB_PASSWORD || null,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
