73493e772703c0c068a7ddf90bc9f4aed20ced88
Tiến Trần <tienco201@gmail.com>
2024-04-09 14:17:54 +0700

    Reminder of vehicle expiration

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dbConnect = require('./config/dbConnect.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const initRoutes = require('./routes');
+const { scheduleJob } = require('./services/emailService.js');
 console.log('cổng: ', process.env.PORT);
 const app = express();
 app.use(
@@ -23,3 +24,8 @@ const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
 });
+
+setInterval(async () => {
+  console.log('Nhắc đi đăng kiểm');
+  await scheduleJob();
+}, 86400000 * 5);
--- services/emailService.js
+++ services/emailService.js
@@ -1,4 +1,6 @@
 const nodemailer = require('nodemailer');
+const db = require('../models');
+const { Op } = require('sequelize');
 
 // Cấu hình transporter của Nodemailer
 const transporter = nodemailer.createTransport({
@@ -30,6 +32,55 @@ const sendEmail = async (recipientEmail, subject, message) => {
   }
 };
 
+// Lập lịch kiểm tra và gửi email thông báo
+const scheduleJob = async () => {
+  const today = new Date();
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 5); // Ngày hết hạn trong tương lai (ví dụ: 5 ngày sau ngày hiện tại)
+
+  try {
+    // Lấy danh sách phương tiện gần hết hạn
+    const vehicles = await db.Vehicle.findAll({
+      where: {
+        expiryDate: {
+          [Op.between]: [today, expirationDate],
+        },
+      },
+
+      include: [
+        {
+          model: db.User,
+          attributes: ['fullname', 'email'],
+        },
+      ],
+    });
+
+    // Gửi email thông báo cho chủ sở hữu phương tiện
+    for (const vehicle of vehicles) {
+      const mailOptions = {
+        from: '"DKSMART" <tienco201@gmail.com>',
+        to: vehicle?.User?.email,
+        subject: `Thông báo: Phương tiện ${vehicle.licensePlate} sắp hết hạn đăng kiểm`,
+        html: `
+        <p>
+          <b>Xin chào ${vehicle?.User?.fullname}</b>
+        </p>
+        <p>Phương tiện của bạn có biển số <b>${vehicle.licensePlate}</b> sắp hết hạn vào ngày ${vehicle.expiryDate}</p>
+        <p>Vui lòng làm mới đăng kiểm kịp thời.</p>
+        <p>Trân trọng</p>
+        `,
+      };
+
+      await transporter.sendMail(mailOptions);
+    }
+
+    console.log('Emails sent successfully.');
+  } catch (error) {
+    console.error('Error sending emails:', error);
+  }
+};
+
 module.exports = {
   sendEmail,
+  scheduleJob,
 };
